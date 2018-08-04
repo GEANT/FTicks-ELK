b2d160ff9945610bd1ffd25ab90c2773ceb811e3
Boro Jakimovski <boro.jakimovski@finki.ukim.mk>
2018-08-04 15:04:30 +0000

    This is the initial dockerized National FTICKS collector.
    It collects FTICKS and forwards them to the central node without filtering.

---
+++ haproxy/config/docker-override-entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+waitport() {
+    while ! nc -z $1 $2 ; do sleep 1 ; done
+}
+
+waitport elasticsearch 9200
+waitport kibana 5601
+
+curl -X PUT -H 'Content-Type: application/json' http://elasticsearch:9200/_template/fticks_template -d '@/fticks.template'
+
+curl -X PUT -H 'Content-Type: application/json' http://elasticsearch:9200/fticks -d '@/fticks.index'
+
+curl -f -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
+  "http://kibana:5601/api/saved_objects/index-pattern/fticks*" \
+  -d"{\"attributes\":{\"title\":\"fticks*\",\"timeFieldName\":\"@timestamp\"}}"
+
+curl -XPOST -H "Content-Type: application/json" -H "kbn-xsrf: anything" \
+  "http://kibana:5601/api/kibana/settings/defaultIndex" \
+  -d"{\"value\":\"fticks*\"}"
+
+/docker-entrypoint.sh haproxy -f /usr/local/etc/haproxy/haproxy.cfg
+
